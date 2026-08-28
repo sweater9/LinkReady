d1d9063a9871246ee13e228458621c9aedaf9bdc
sweater9 <sweater.9arc@icloud.com>
2026-08-28 19:39:49 +0400

    fix: harden QR tools and add URL editor

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='linkready-v3';
+const CACHE='linkready-v4';
 const ASSETS=['./','./index.html','./styles.css','./cleaner.js','./app.js','./manifest.webmanifest','./icon.svg','./apple-touch-icon.png','./og.png','./vendor/qrcode.min.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
